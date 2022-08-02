393793fb5edfc7bdc31b33d2ee88b03e2424c560
秉烛夜游 <dcf_t@192.168.0.107>
2022-08-02 17:39:34 +0800

    A

--- Human.cpp
+++ Human.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 Human::Human(){
     _choice = 0;
-    cout<<"Please enter R, P or S:"<<endl;
-    cin>>_choice;
 }
 char Human::makeMove(){
+    cout<<"Please enter R, P or S:"<<endl;
+    cin>>_choice;
     return _choice;
 }
