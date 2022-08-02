c7724326edc47a7db5050990b59a4d805c19b144
秉烛夜游 <dcf_t@192.168.0.107>
2022-08-02 17:54:36 +0800

    A

--- Human.cpp
+++ Human.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 Human::Human(){
     _choice = 0;
-}
-char Human::makeMove(){
     cout<<"Enter move: "<<endl;
     cin>>_choice;
+}
+char Human::makeMove(){
     return _choice;
 }
