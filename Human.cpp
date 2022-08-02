7e59520430e2584a5d3b092109db411c3a9d4a39
秉烛夜游 <dcf_t@192.168.0.107>
2022-08-02 17:34:37 +0800

    1

--- Human.cpp
+++ Human.cpp
@@ -3,7 +3,6 @@
 
 using namespace std;
 
-
 Human::Human(){
     cout<<"Please enter R, P or S:"<<endl;
     cin>>_choice;
--- Referee.cpp
+++ Referee.cpp
@@ -1,62 +1,65 @@
-#include <iostream>
 #include "Referee.h"
-#include "Human.h"
-#include "Computer.h"
 
-Referee::Referee(){
-    _result=0;
-}
-char Referee::refGame(Human  player1, Computer player2){
-    if(player1.makeMove() == 'R'){
-        switch (player2.makeMove())
-        {
+#include <iostream>
+
+#include "Computer.h"
+#include "Human.h"
+
+Referee::Referee() { _result = 0; }
+char Referee::refGame(Human player1, Computer player2) {
+  while (1 == 1) {
+    if (player1.makeMove() == 'R') {
+      switch (player2.makeMove()) {
         case 'R':
-            _result='T';
-            break;
+          _result = 'T';
+          break;
         case 'S':
-            _result='W';
-            break;
+          _result = 'W';
+          break;
         case 'P':
-            _result='L';
-            break;
+          _result = 'L';
+          break;
         default:
-            _result=0;
-            break;
-        }
+          _result = 0;
+          break;
+      }
+      return _result;
     }
-    if(player1.makeMove() == 'P'){
-        switch (player2.makeMove())
-        {
+    if (player1.makeMove() == 'P') {
+      switch (player2.makeMove()) {
         case 'P':
-            _result='T';
-            break;
+          _result = 'T';
+          break;
         case 'R':
-            _result='W';
-            break;
+          _result = 'W';
+          break;
         case 'S':
-            _result='L';
-            break;
+          _result = 'L';
+          break;
         default:
-            _result=0;
-            break;
-        }
+          _result = 0;
+          break;
+      }
+      return _result;
     }
-    if(player1.makeMove() == 'S'){
-        switch (player2.makeMove())
-        {
+    if (player1.makeMove() == 'S') {
+      switch (player2.makeMove()) {
         case 'S':
-            _result='T';
-            break;
+          _result = 'T';
+          break;
         case 'P':
-            _result='W';
-            break;
+          _result = 'W';
+          break;
         case 'R':
-            _result='L';
-            break;
+          _result = 'L';
+          break;
         default:
-            _result=0;
-            break;
-        }
+          _result = 0;
+          break;
+      }
+      return _result;
     }
-    return _result;
+  }
+
+  return _result;
 }
