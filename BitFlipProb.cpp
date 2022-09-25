f21c14e6b5d40b7f5f041d2d388cc7b666fcd7eb
ChufanDong <1823303255@qq.com>
2022-09-25 23:57:33 +0800

    a

--- BitFlipProb.cpp
+++ BitFlipProb.cpp
@@ -12,7 +12,7 @@ Individual BitFlipProb::mutate(Individual ind, int k)
     
     for (int i = 0; i < ind.getLength(); i++)
     {
-        double a = rand() % 10 / 10;
+        double a = rand() / double(1.0);
         if (a <= prob)
         {
             ind.flipBit(i);
