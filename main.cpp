8e2fa1205e734f8a30e77184fca8e0dfb56926ea
ChufanDong <1823303255@qq.com>
2022-10-22 23:31:47 +1030

    done

--- main.cpp
+++ main.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <stack>
-#include <string> 
+#include <string>
 
 using namespace std;
 
-string SwitchToMid(string str){
+string SwitchToMid(vector<string> str){
     bool again = false;
-    string oper, nums, result;
-    for(int i = 0; i < str.length(); i++){
-        if(str[i] == '+' ||str[i] == '-' ||str[i] == '*' ||str[i] == '/'){}
+    vector<string> oper, nums;
+    string result;
+
+    for(int i = 0; i < str.size(); i++){
+        if(str[i] == "+" ||str[i] == "-" ||str[i] == "*" ||str[i] == "/"){}
         else{
-            oper.assign(str, 0, i);
-            str.erase(0, i);
+            oper.assign(str.begin(),str.begin()+i);
+            str.erase(str.begin(),str.begin()+i);
             break;
         }
     }
 
-    for(int i = 0; i < str.length(); i++){
-        if(i+1==str.length()){
-            nums.assign(str);
-            str = "";
+    for(int i = 0; i < str.size(); i++){
+        if(i+1==str.size()){
+            nums.assign(str.begin(),str.end());
+            str.erase(str.begin(),str.end());
             break;
         }
-        else if(str[i] >= '0' && str[i] <= '9'){
+        else if(stoi(str[i]) >= 0 && stoi(str[i]) <= 99){
         }
         else{
-            nums.assign(str, 0, i);
-            str.erase(0, i);
+            nums.assign(str.begin(),str.begin()+i);
+            str.erase(str.begin(),str.begin()+i);
             again = true;
             break;
         }
     }
+
     
-    int size_oper = oper.length()-1;
+    int size_oper = oper.size()-1;
     int size_nums = 0;
 
     while( 1 ){
-        if (size_nums != nums.length()){
+        if (size_nums != nums.size()){
             result += nums[size_nums];
             size_nums++;
         }
         if(size_oper >= 0){
-            if (oper[size_oper] == '*' || oper[size_oper] == '/'){
-                if(oper[size_oper+1] == '+' || oper[size_oper+1] == '-'){
+            if (oper[size_oper] == "*" || oper[size_oper] == "/"){
+                if(oper[size_oper+1] == "+" || oper[size_oper+1] == "-"){
                     result = result.insert(0,"(");
                     result += ')';
                     result += oper[size_oper];
@@ -69,7 +72,7 @@ string SwitchToMid(string str){
     return result;
 }
 
-string calculator(string str){
+string calculator(vector<string> str){
     string result;
 	stack<int> mysk_num;
 	stack<char> mysk_oper;
@@ -78,15 +81,13 @@ string calculator(string str){
 	int num1, num2, num3;
 	
 	for(int i=size-1; i>=0; i--){
-		if(str[i] >= '0' && str[i] <= '9') 
-			mysk_num.push(str[i]-'0');
-		else{
+        if(str[i]=="+"||str[i]=="-"||str[i]=="*"||str[i]=="/"){
             if (!mysk_num.empty()){
                 num1 = mysk_num.top();
 			    mysk_num.pop();
             }
             else{
-                cout<<"Error"<<endl;
+                std::cout<<"Error"<<endl;
                 return 0;
             }
 			if (!mysk_num.empty()){
@@ -97,26 +98,32 @@ string calculator(string str){
                 return "Error";
             }
 			
-			if(str[i] == '+') num3 = num1 + num2;
-			else if(str[i] == '-') num3 = num1 - num2;
-			else if(str[i] == '*') num3 = num1 * num2;
-			else if(str[i] == '/') num3 = num1 / num2;
+			if(str[i] == "+") num3 = num1 + num2;
+			else if(str[i] == "-") num3 = num1 - num2;
+			else if(str[i] == "*") num3 = num1 * num2;
+			else if(str[i] == "/") num3 = num1 / num2;
 			
 			mysk_num.push(num3);
-		}
+        }
+		else{
+            mysk_num.push(stoi(str[i]));
+        }
 	}
-    cout<<SwitchToMid(str)<<" = ";
+    std::cout<<SwitchToMid(str)<<" = ";
     result = to_string(mysk_num.top());
     return result;
 }
 
 int main(){
-    string Input;
-    getline(cin, Input);
-    for(int i = 0; i < Input.length(); i++){
-        if (Input[i] == ' ') Input.erase(i,1);
+    vector<string> Input;
+    string getEnter;
+    while (cin>>getEnter){
+        Input.push_back(getEnter);
+        if(cin.get() == '\n'){
+            break;
+        }
     }
-    cout<<calculator(Input)<<endl;
+    std::cout<<calculator(Input)<<endl;
 
     return 0;
 }
